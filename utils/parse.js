8a08528eef86ac366582fef169f558ed2c7154e4
Max Komarychev <maxkomarychev@gmail.com>
2020-02-24 08:35:38 +0200

    wip

--- utils/parse.js
+++ utils/parse.js
@@ -3,70 +3,20 @@ exports.__esModule = true
 
 const moduleRequire = require('./module-require').default
 const extname = require('path').extname
+const fs = require('fs')
 
 const log = require('debug')('eslint-plugin-import:parse')
 
-exports.default = function parse(path, content, context) {
-
-  if (context == null) throw new Error('need context to parse properly')
-
-  let parserOptions = context.parserOptions
+function getBabelVisitorKeys(path) {
   const parserPath = getParserPath(path, context)
-
-  if (!parserPath) throw new Error('parserPath is required!')
-
-  // hack: espree blows up with frozen options
-  parserOptions = Object.assign({}, parserOptions)
-  parserOptions.ecmaFeatures = Object.assign({}, parserOptions.ecmaFeatures)
-
-  // always include comments and tokens (for doc parsing)
-  parserOptions.comment = true
-  parserOptions.attachComment = true  // keeping this for backward-compat with  older parsers
-  parserOptions.tokens = true
-
-  // attach node locations
-  parserOptions.loc = true
-  parserOptions.range = true
-
-  // provide the `filePath` like eslint itself does, in `parserOptions`
-  // https://github.com/eslint/eslint/blob/3ec436ee/lib/linter.js#L637
-  parserOptions.filePath = path
-  
-  // @typescript-eslint/parser will parse the entire project with typechecking if you provide
-  // "project" or "projects" in parserOptions. Removing these options means the parser will
-  // only parse one file in isolate mode, which is much, much faster.
-  // https://github.com/benmosher/eslint-plugin-import/issues/1408#issuecomment-509298962
-  delete parserOptions.project
-  delete parserOptions.projects
-  
-  // require the parser relative to the main module (i.e., ESLint)
-  const parser = moduleRequire(parserPath)
-  console.log('parser', parserPath, Object.keys(parser))
-  console.log(JSON.stringify(parser))
-
-  if (typeof parser.parseForESLint === 'function') {
-    console.log('has parse for estlint')
-    let ast
-    try {
-      const parserRaw = parser.parseForESLint(content, parserOptions)
-      console.log('parser output', Object.keys(parserRaw))
-      console.log(JSON.stringify(parserRaw))
-      ast = parserRaw.ast
-    } catch (e) {
-      //
-    }
-    if (!ast || typeof ast !== 'object') {
-      console.warn(
-        '`parseForESLint` from parser `' +
-          parserPath +
-          '` is invalid and will just be ignored'
-      )
-    } else {
-      return ast
-    }
+  const hypotheticalLocation = parserPath.replace('index.js', 'visitor-keys.js')
+  if (fs.existsSync(hypotheticalLocation)) {
+    const keys = moduleRequire(parserPath.replace('index.js', 'visitor-keys.js'))
+    return keys
+  } else {
+    return null
   }
-
-  return parser.parse(content, parserOptions)
+  // __visit(ast, keys, visitorSpec)
 }
 
 function __visit(node, keys, visitorSpec) {
@@ -101,6 +51,76 @@ exports.visit = function (ast, path, context, visitorSpec) {
   const keys = moduleRequire(parserPath.replace('index.js', 'visitor-keys.js'))
   __visit(ast, keys, visitorSpec)
 }
+
+
+exports.default = function parse(path, content, context) {
+
+  if (context == null) throw new Error('need context to parse properly')
+
+  let parserOptions = context.parserOptions
+  const parserPath = getParserPath(path, context)
+
+  if (!parserPath) throw new Error('parserPath is required!')
+
+  // hack: espree blows up with frozen options
+  parserOptions = Object.assign({}, parserOptions)
+  parserOptions.ecmaFeatures = Object.assign({}, parserOptions.ecmaFeatures)
+
+  // always include comments and tokens (for doc parsing)
+  parserOptions.comment = true
+  parserOptions.attachComment = true  // keeping this for backward-compat with  older parsers
+  parserOptions.tokens = true
+
+  // attach node locations
+  parserOptions.loc = true
+  parserOptions.range = true
+
+  // provide the `filePath` like eslint itself does, in `parserOptions`
+  // https://github.com/eslint/eslint/blob/3ec436ee/lib/linter.js#L637
+  parserOptions.filePath = path
+  
+  // @typescript-eslint/parser will parse the entire project with typechecking if you provide
+  // "project" or "projects" in parserOptions. Removing these options means the parser will
+  // only parse one file in isolate mode, which is much, much faster.
+  // https://github.com/benmosher/eslint-plugin-import/issues/1408#issuecomment-509298962
+  delete parserOptions.project
+  delete parserOptions.projects
+  
+  let visitorKeys = null
+  // require the parser relative to the main module (i.e., ESLint)
+  const parser = moduleRequire(parserPath)
+  // if parser is estree
+  visitorKeys = parser.VisitorKeys
+  console.log('parser', parserPath, Object.keys(parser))
+  console.log(JSON.stringify(parser))
+
+  if (typeof parser.parseForESLint === 'function') {
+    console.log('has parse for estlint')
+    let ast
+    try {
+      const parserRaw = parser.parseForESLint(content, parserOptions)
+      // if parser is @typescript-eslint/parser
+      visitorKeys = parserRaw.visitorKeys
+      console.log('parser output', Object.keys(parserRaw))
+      console.log(JSON.stringify(parserRaw))
+      ast = parserRaw.ast
+    } catch (e) {
+      //
+    }
+    if (!ast || typeof ast !== 'object') {
+      console.warn(
+        '`parseForESLint` from parser `' +
+          parserPath +
+          '` is invalid and will just be ignored'
+      )
+    } else {
+      return ast
+    }
+  }
+
+  return parser.parse(content, parserOptions)
+}
+
 
 function getParserPath(path, context) {
   const parsers = context.settings['import/parsers']
