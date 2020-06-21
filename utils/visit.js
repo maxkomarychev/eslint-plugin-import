56c5245ffbe19e40d4338abe2beafb66b739cedd
Max Komarychev <maxkomarychev@gmail.com>
2020-06-21 11:36:11 +0300

    Use strict

--- utils/visit.js
+++ utils/visit.js
@@ -1,28 +1,29 @@
+'use strict'
 exports.__esModule = true
 
 exports.default = function visit(node, keys, visitorSpec) {
   if (!node || !keys) {
     return
   }
-    const type = node.type
-    if (typeof visitorSpec[type] === 'function') {
-      visitorSpec[type](node)
-    }
-    const childFields = keys[type]
-    if (!childFields) {
-      return
-    }
-    for (const fieldName of childFields) {
-      const field = node[fieldName]
-      if (Array.isArray(field)) {
-        for (const item of field) {
-          visit(item, keys, visitorSpec)
-        }
-      } else {
-        visit(field, keys, visitorSpec)
+  const type = node.type;
+  if (typeof visitorSpec[type] === 'function') {
+    visitorSpec[type](node)
+  }
+  const childFields = keys[type]
+  if (!childFields) {
+    return
+  }
+  for (const fieldName of childFields) {
+    const field = node[fieldName]
+    if (Array.isArray(field)) {
+      for (const item of field) {
+        visit(item, keys, visitorSpec)
       }
+    } else {
+      visit(field, keys, visitorSpec)
     }
-    if (typeof visitorSpec[`${type}:Exit`] === 'function') {
-      visitorSpec[`${type}:Exit`](node)
-    }
+  }
+  if (typeof visitorSpec[`${type}:Exit`] === 'function') {
+    visitorSpec[`${type}:Exit`](node)
+  }
 }
