c10e2172c533fc29f7404e6cd4cc628ac3df5f8e
Max Komarychev <maxkomarychev@gmail.com>
2020-06-24 23:02:58 +0300

    Replace loops with foreach

--- utils/visit.js
+++ utils/visit.js
@@ -13,16 +13,11 @@ exports.default = function visit(node, keys, visitorSpec) {
   if (!childFields) {
     return
   }
-  for (const fieldName of childFields) {
-    const field = node[fieldName]
-    if (Array.isArray(field)) {
-      for (const item of field) {
-        visit(item, keys, visitorSpec)
-      }
-    } else {
-      visit(field, keys, visitorSpec)
-    }
-  }
+  childFields.forEach((fieldName) => {
+    [].concat(node[fieldName]).forEach((item) => {
+      visit(item, keys, visitorSpec)
+    })
+  })
   if (typeof visitorSpec[`${type}:Exit`] === 'function') {
     visitorSpec[`${type}:Exit`](node)
   }
